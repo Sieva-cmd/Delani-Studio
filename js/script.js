31437c7996749b33d82d78ce412f5539fd58bab1
sieva-cmd <musyokasieva@gmail.com>
2021-12-18 00:22:51 +0300

    add toggle effect to design section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
-  
+    $("#designImage").click(function() {
+        $("#designP").toggle();
+        $("#designImage").hide();
+        
+    });
+ 
+    $("#designP").click(function() {
+     $("#designImage").toggle();
+     $("#designP").hide();
+    })
  
  });
  
