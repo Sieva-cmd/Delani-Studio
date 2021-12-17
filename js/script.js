9d47d521df1d075888295c316c6a6b7265b709b8
sieva-cmd <musyokasieva@gmail.com>
2021-12-18 00:33:52 +0300

    add toggle effect to development section

--- js/script.js
+++ js/script.js
@@ -8,7 +8,16 @@ $(document).ready(function () {
     $("#designP").click(function() {
      $("#designImage").toggle();
      $("#designP").hide();
-    })
+    });
+
+    $("#devImage").click(function() {
+        $("#devP").toggle();
+        $("#devImage").hide();
+    });
+    $("#devP").click(function() {
+        $("#devImage").toggle();
+        $("#devP").hide();
+    });
  
  });
  
