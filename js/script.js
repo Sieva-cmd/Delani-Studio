d8c03c9714f0da52befe76b9bdb9ef4ee31cd699
sieva-cmd <musyokasieva@gmail.com>
2021-12-18 11:42:47 +0300

    adding a section with project name and creating hover effect on the projects

--- js/script.js
+++ js/script.js
@@ -26,6 +26,30 @@ $(document).ready(function () {
         $("#productImage").toggle();
         $("#productP").hide();
     });
- 
+    $("#work4").hover(function() {
+      $(".name1").toggle(); 
+        
+    });
+    $("#work3").hover(function() { 
+        $(".name2").toggle();  
+      });
+      $("#work2").hover(function() { 
+        $(".name3").toggle();  
+      });
+      $("#work1").hover(function() { 
+        $(".name4").toggle();  
+      });
+      $("#work5").hover(function() { 
+        $(".name5").toggle();  
+      });
+      $("#work6").hover(function() { 
+        $(".name6").toggle();  
+      });
+      $("#work7").hover(function() { 
+        $(".name7").toggle();  
+      });
+      $("#work8").hover(function() { 
+        $(".name8").toggle();  
+      });
+
  });
- 
